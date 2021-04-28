83be2aeae9dacd793f6ae92ba160b2f798e3af00
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-04-28 21:09:07 +0200

    Add TODOModel.java

---
+++ app/src/main/java/com/mahmoudramadan/todolist/Model/TODOModel.java
@@ -0,0 +1,38 @@
+package com.mahmoudramadan.todolist.Model;
+
+public class TODOModel {
+    private int id, status;
+    private String task, date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
