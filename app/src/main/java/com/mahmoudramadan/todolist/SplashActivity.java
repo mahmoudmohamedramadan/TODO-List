f8a616c12a50150a5285292433c64c3807c66ca6
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-04-28 21:03:23 +0200

    Add SplashActivity.java

---
+++ app/src/main/java/com/mahmoudramadan/todolist/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.mahmoudramadan.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(() -> {
+            startActivity(intent);
+            finish();
+        }, 4000);
+    }
+}
