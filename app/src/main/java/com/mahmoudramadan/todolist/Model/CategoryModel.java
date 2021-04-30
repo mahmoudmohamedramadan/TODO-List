9ff59c09a2e6ce7f1aa2ff0c4189c9bfa2cad90e
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-04-30 21:18:28 +0200

    Add CategoryModel.java

---
+++ app/src/main/java/com/mahmoudramadan/todolist/Model/CategoryModel.java
@@ -0,0 +1,23 @@
+package com.mahmoudramadan.todolist.Model;
+
+public class CategoryModel {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private String category;
+}
