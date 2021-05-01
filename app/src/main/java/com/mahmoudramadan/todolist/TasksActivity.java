318d8dcf652a1e5ae668ea904d2ca7247a7e4fb7
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-05-01 19:20:17 +0200

    Show category name and refactoring TasksActivity.java

--- app/src/main/java/com/mahmoudramadan/todolist/TasksActivity.java
+++ app/src/main/java/com/mahmoudramadan/todolist/TasksActivity.java
@@ -3,6 +3,7 @@ package com.mahmoudramadan.todolist;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.ItemTouchHelper;
@@ -20,11 +21,9 @@ import java.util.List;
 
 public class TasksActivity extends AppCompatActivity implements DialogCloseListener {
 
-    private RecyclerView tasksRecycleView;
     private TODOAdapter tasksAdapter;
     private List<TODOModel> taskList;
     private TODODatabaseHandler db;
-    private FloatingActionButton addNewTaskButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +37,17 @@ public class TasksActivity extends AppCompatActivity implements DialogCloseListe
 
         taskList = new ArrayList<>();
 
-        tasksRecycleView = findViewById(R.id.tasksRecycleView);
+        RecyclerView tasksRecycleView = findViewById(R.id.tasksRecycleView);
         tasksRecycleView.setLayoutManager(new LinearLayoutManager(this));
         tasksAdapter = new TODOAdapter(db, this);
         tasksRecycleView.setAdapter(tasksAdapter);
 
-        addNewTaskButton = findViewById(R.id.addNewTaskButton);
+        FloatingActionButton addNewTaskButton = findViewById(R.id.addNewTaskButton);
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new TODORecyclerItemTouchHelper(tasksAdapter));
         itemTouchHelper.attachToRecyclerView(tasksRecycleView);
+
+        TextView taskNameEditText = findViewById(R.id.taskNameEditText);
+        taskNameEditText.setText(getIntent().getStringExtra("category"));
 
         taskList = db.getTasks(getIntent().getStringExtra("category_id"));
         Collections.reverse(taskList);
