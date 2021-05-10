f5a9201f08c033c501b004c4e09da7c77710b05c
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-05-11 00:53:32 +0200

    Customize NotificationPublisher.java

--- app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationPublisher.java
+++ app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationPublisher.java
@@ -8,16 +8,12 @@ import android.content.Intent;
 
 public class NotificationPublisher extends BroadcastReceiver {
 
-    public static String NOTIFICATION_ID = "notification-id";
     public static String NOTIFICATION = "notification";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
-        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
-        notificationManager.notify(id, notification);
+        notificationManager.notify((int) System.currentTimeMillis(), notification);
     }
 }
