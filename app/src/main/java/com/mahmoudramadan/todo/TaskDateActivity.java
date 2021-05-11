e2d386fd66b51398d7daf2887de271cbbf218b4a
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-05-11 21:46:30 +0200

    Customize TaskDateActivity.java

--- app/src/main/java/com/mahmoudramadan/todo/TaskDateActivity.java
+++ app/src/main/java/com/mahmoudramadan/todo/TaskDateActivity.java
@@ -4,16 +4,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.mahmoudramadan.todo.Utils.TODODatabaseHandler;
@@ -21,7 +18,6 @@ import com.mahmoudramadan.todo.Utils.TODODatabaseHandler;
 public class TaskDateActivity extends AppCompatDialogFragment {
 
     public static String TAG = "TaskDateDialog";
-    private Button saveTaskDateTimeButton;
     private DatePicker taskDatePicker;
     private TimePicker taskTimePicker;
     private View currentClickedItem;
@@ -37,33 +33,15 @@ public class TaskDateActivity extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.task_date, null);
-
-        saveTaskDateTimeButton = view.findViewById(R.id.saveTaskDateTimeButton);
-        taskDatePicker = view.findViewById(R.id.taskDatePicker);
-        taskTimePicker = view.findViewById(R.id.taskTimePicker);
-        taskTimePicker.setIs24HourView(true);
-
-        builder.setView(view).setCustomTitle(new TextView(getContext()));
-        return builder.create();
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
         TODODatabaseHandler db = new TODODatabaseHandler(getActivity());
         db.openDatabase();
 
         final Bundle bundle = getArguments();
 
-        saveTaskDateTimeButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setPositiveButton(getString(R.string.save), new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(DialogInterface dialog, int which) {
                 TextView selectedDateTimeTextView = currentClickedItem.findViewById(R.id.selectedDateTimeTextView);
                 int monthDate = taskDatePicker.getMonth() + 1;
                 String dateTime = taskDatePicker.getYear() + "/" + monthDate + "/" + taskDatePicker.getDayOfMonth() + " " +
@@ -76,6 +54,22 @@ public class TaskDateActivity extends AppCompatDialogFragment {
                 dismiss();
             }
         });
+        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.task_date, null);
+
+        taskDatePicker = view.findViewById(R.id.taskDatePicker);
+        taskTimePicker = view.findViewById(R.id.taskTimePicker);
+        taskTimePicker.setIs24HourView(true);
+
+        builder.setView(view).setCustomTitle(new TextView(getContext()));
+        return builder.create();
     }
 
     @Override
