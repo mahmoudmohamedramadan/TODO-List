3c93aeb8d1b24bfaf3857558a815c12544d0394c
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-05-11 13:30:55 +0200

    Customize NotificationManager.java

--- app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationManager.java
+++ app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationManager.java
@@ -1,15 +1,11 @@
 package com.mahmoudramadan.todo.Notifications;
 
 import android.app.AlarmManager;
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 
 import com.mahmoudramadan.todo.MainActivity;
-import com.mahmoudramadan.todo.R;
-import com.mahmoudramadan.todo.TasksActivity;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -18,28 +14,20 @@ import java.util.Locale;
 
 public class NotificationManager {
 
-    public static void scheduleNotification(Notification notification, MainActivity activity, String dateTime) throws ParseException {
+    public static void scheduleNotification(MainActivity activity, String[] taskData) throws ParseException {
         Intent notificationIntent = new Intent(activity, NotificationPublisher.class);
-        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        notificationIntent.putExtra("task", taskData[0]);
+        notificationIntent.putExtra("category_id", taskData[2]);
 
         SimpleDateFormat sDFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.ENGLISH);
 
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(sDFormat.parse(dateTime));
+        calendar.setTime(sDFormat.parse(taskData[1]));
 
         PendingIntent pendingIntent =
                 PendingIntent.getBroadcast(activity, (int) System.currentTimeMillis(), notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         AlarmManager alarmManager = (AlarmManager) activity.getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-    }
-
-    public static Notification getNotification(String content, MainActivity activity) {
-        Notification.Builder builder = new Notification.Builder(activity)
-                .setContentTitle(activity.getString(R.string.app_name))
-                .setContentText(content)
-                .setSmallIcon(R.mipmap.ic_launcher_todo)
-                .setLargeIcon(BitmapFactory.decodeResource(activity.getResources(), R.mipmap.ic_launcher_todo));
-        return builder.build();
     }
 }
