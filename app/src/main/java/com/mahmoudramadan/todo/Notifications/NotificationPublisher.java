031135a8ff5f8ad7aa7f9f4164b7a1b7c1f9b678
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-05-13 14:38:11 +0200

    Fix bug of pushing notifications

--- app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationPublisher.java
+++ app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationPublisher.java
@@ -1,21 +1,29 @@
 package com.mahmoudramadan.todo.Notifications;
 
 import android.annotation.SuppressLint;
-import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.AudioManager;
+import android.os.Build;
 import android.provider.Settings;
+import android.provider.SyncStateContract;
+
+import androidx.core.app.NotificationCompat;
 
 import com.mahmoudramadan.todo.R;
 import com.mahmoudramadan.todo.TasksActivity;
 import com.mahmoudramadan.todo.Utils.CategoryDatabaseHandler;
 
 public class NotificationPublisher extends BroadcastReceiver {
+
+    String CHANNEL_ID = SyncStateContract.Constants._ID;
+    String CHANNEL_NAME = "TODO";
 
     @Override
     @SuppressLint("WrongConstant")
@@ -31,17 +39,30 @@ public class NotificationPublisher extends BroadcastReceiver {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(),
                 tasksActivityIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        Notification notification = new Notification.Builder(context)
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableVibration(true);
+            channel.setLightColor(Color.BLUE);
+            channel.enableLights(true);
+            if (channel.canShowBadge()) channel.setShowBadge(true);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setContentTitle(context.getString(R.string.app_name))
                 .setContentText(intent.getStringExtra("task"))
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
                 .setSound(Settings.System.DEFAULT_NOTIFICATION_URI, AudioManager.STREAM_NOTIFICATION)
                 .setSmallIcon(R.drawable.ic_baseline_notifications)
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher_todo))
-                .build();
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher_todo));
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify((int) System.currentTimeMillis(), notification);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notification.setChannelId(CHANNEL_ID);
+        }
+
+        notificationManager.notify((int) System.currentTimeMillis(), notification.build());
     }
 }
