5173e22845e9eb1d25310bde95ad18ecdeadcf39
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-05-14 15:16:05 +0200

    Get song saved in preferences

--- app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationPublisher.java
+++ app/src/main/java/com/mahmoudramadan/todo/Notifications/NotificationPublisher.java
@@ -7,11 +7,11 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Build;
-import android.provider.Settings;
 import android.provider.SyncStateContract;
 
 import androidx.core.app.NotificationCompat;
@@ -41,6 +41,9 @@ public class NotificationPublisher extends BroadcastReceiver {
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        SharedPreferences preferences = context.getSharedPreferences("Settings", context.MODE_PRIVATE);
+        String song = preferences.getString("song", "");
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
             channel.enableVibration(true);
@@ -55,7 +58,7 @@ public class NotificationPublisher extends BroadcastReceiver {
                 .setContentText(intent.getStringExtra("task"))
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
-                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI, AudioManager.STREAM_NOTIFICATION)
+                .setSound(Uri.parse(song))
                 .setSmallIcon(R.drawable.ic_baseline_notifications)
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher_todo));
 
